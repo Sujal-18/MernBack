ee585368db2b52b1cf042404e80fdb9466d90af2
Sujal <sujalphuse@gmail.com>
2024-07-07 23:14:20 +0530

    first commit

---
+++ routes/transactions.js
@@ -0,0 +1,12 @@
+const { addExpense, getExpense, deleteExpense } = require('../controllers/expense');
+const { addIncome,getIncomes,deleteIncome } = require('../controllers/income');
+
+const router = require('express').Router();
+
+router.post('/add-incomes',addIncome)
+.get('/get-incomes',getIncomes)
+.delete('/delete-incomes/:id',deleteIncome)
+.post('/add-expenses',addExpense)
+.get('/get-expenses',getExpense)
+.delete('/delete-expenses/:id',deleteExpense)
+module.exports = router 
